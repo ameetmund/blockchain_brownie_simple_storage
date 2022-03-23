c06382d6e90c11505963da23ac93809d622732f5
Ameet Mund <ameetmund@gmail.com>
2022-03-23 06:37:14 +0530

    Added new contract SimpleStorage.sol

---
+++ contracts/SimpleStorage.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity =0.6.0;
+
+/**
+ * @title Storage
+ * @dev Store & retrieve value in a variable
+ */
+contract Storage {
+    uint256 favNumber;
+
+    struct People {
+        uint256 favNumber;
+        string name;
+    }
+
+    People[] public people;
+    mapping(string => uint256) public nameToFavNumber;
+
+    // /**
+    //  * @dev Store value in variable
+    //  * @param num value to store
+    //  */
+    function store(uint256 _favNumber) public returns (uint256) {
+        favNumber = _favNumber;
+        return _favNumber;
+    }
+
+    /**
+     * @dev Return value
+     * @return value of 'number'
+     */
+    function retrieve() public view returns (uint256) {
+        return favNumber;
+    }
+
+    function addPerson(string memory _name, uint256 _favNumber) public {
+        people.push(People(_favNumber, _name));
+        nameToFavNumber[_name] = _favNumber;
+    }
+}
